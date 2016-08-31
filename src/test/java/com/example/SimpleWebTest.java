ec94253c02660b10bbdca9005808d70babb5559b
Douglas Man <douglas.man@me.com>
2016-08-31 14:09:42 -0400

    pageNotFound...

--- src/test/java/com/example/SimpleWebTest.java
+++ src/test/java/com/example/SimpleWebTest.java
@@ -2,11 +2,14 @@ package com.example;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.WebIntegrationTest;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.web.client.HttpClientErrorException;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 import static junit.framework.TestCase.assertEquals;
@@ -20,14 +23,20 @@ import static org.junit.Assert.fail;
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class SimpleWebTest {
 
-    @Test(expected=HttpClientErrorException.class)
+    @Autowired
+    private TestRestTemplate rest;
+
+//    @Test(expected=HttpClientErrorException.class)
+    @Test(expected = RestClientException.class)
     public void pageNotFound() {
         try {
-            RestTemplate rest = new RestTemplate();
-            rest.getForObject("http://localhost:8080/bogusPage", String.class);
+//            RestTemplate rest = new RestTemplate();
+            rest.getForObject("/bogusPage", String.class);
+//            fail();
             fail("Should result in HTTP 404");
-        } catch (HttpClientErrorException e) {
-            assertEquals(HttpStatus.NOT_FOUND, e.getStatusCode());
+//        } catch (HttpClientErrorException e) {
+        } catch (RestClientException e) {
+            assertEquals(HttpStatus.NOT_FOUND, e.getMessage());
             throw e;
         }
     }
